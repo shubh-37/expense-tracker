873bfd0aba75b52a01ebb8c3dc77b3bf5d6390d3
Shubh Arya <shubh.arya37@gmail.com>
2024-04-30 16:46:47 +0530

    created a widget to add a new expense

---
+++ lib/widgets/create_new_expense.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class CreateNewExpense extends StatefulWidget {
+  const CreateNewExpense({super.key});
+  @override
+  State<CreateNewExpense> createState() {
+    return _CreateNewState();
+  }
+}
+
+class _CreateNewState extends State<CreateNewExpense> {
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+  @override
+  void dispose() {
+    _amountController.dispose();
+    _titleController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(context) {
+    return Padding(
+      padding: const EdgeInsets.all(5),
+      child: Column(
+        children: [
+          TextField(
+            controller: _titleController,
+            maxLength: 50,
+            decoration: const InputDecoration(label: Text("Title")),
+          ),
+          TextField(
+            controller: _amountController,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+                prefixText: "Rs. ", label: Text("Amount")),
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    print(_titleController.text);
+                    print(_amountController.text);
+                  },
+                  child: const Text('Submit')),
+              ElevatedButton(onPressed: () {}, child: const Text('Cancel'))
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}

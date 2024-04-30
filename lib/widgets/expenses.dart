88b3bb2d5bfa5ed3018e1a02b814898bf312e3c4
Shubh Arya <shubh.arya37@gmail.com>
2024-04-30 16:46:26 +0530

    calling a new widget on click of the plus icon

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/create_new_expense.dart';
 import 'package:expense_tracker/widgets/expense_list/expenses_list.dart';
 import 'package:flutter/material.dart';
 
@@ -23,12 +24,21 @@ class _ExpensesState extends State<Expenses> {
         date: DateTime.now(),
         category: Category.leisure)
   ];
+
+  void _showAddExpenseOverlay() {
+    showModalBottomSheet(
+        context: context, builder: (ctx) => const CreateNewExpense());
+  }
+
   @override
   Widget build(context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Flutter Expense Tracker"),
-        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.add))],
+        actions: [
+          IconButton(
+              onPressed: _showAddExpenseOverlay, icon: const Icon(Icons.add))
+        ],
       ),
       body: Column(
         children: [

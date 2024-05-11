ff9931f79292d1cb47720a21017923f9f30f4b3f
Shubh Arya <shubh.arya37@gmail.com>
2024-05-11 12:59:52 +0530

    added a dismissible widget to swipe the expense to remve it

--- lib/widgets/expense_list/expenses_list.dart
+++ lib/widgets/expense_list/expenses_list.dart
@@ -3,12 +3,20 @@ import 'package:expense_tracker/widgets/expense_list/expense_item.dart';
 import 'package:flutter/material.dart';
 
 class ExpensesList extends StatelessWidget {
-  const ExpensesList({required this.expenses, super.key});
+  const ExpensesList(
+      {required this.expenses, super.key, required this.removeExpense});
+  final void Function(Expense expense) removeExpense;
   final List<Expense> expenses;
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
         itemCount: expenses.length,
-        itemBuilder: (context, index) => ExpenseItem(expenses[index]));
+        itemBuilder: (context, index) => Dismissible(
+              key: ValueKey(expenses[index]),
+              child: ExpenseItem(expenses[index]),
+              onDismissed: (duration) {
+                removeExpense(expenses[index]);
+              },
+            ));
   }
 }
